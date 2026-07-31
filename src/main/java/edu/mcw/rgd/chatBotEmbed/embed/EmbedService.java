6b066be434e750d33812a7759911341d76a82ca9
llamersmcw <llamers@mcw.edu>
2026-07-31 12:06:53 -0500

    Update EmbedService.java

--- src/main/java/edu/mcw/rgd/chatBotEmbed/embed/EmbedService.java
+++ src/main/java/edu/mcw/rgd/chatBotEmbed/embed/EmbedService.java
@@ -72,7 +72,7 @@ public class EmbedService {
         EmbeddingClient client = new EmbeddingClient(provider, baseUrl, model, dimensions, apiKey);
         DocumentEmbeddingDAO embeddingDAO = new DocumentEmbeddingDAO();
         EmbeddingWriter writer = new EmbeddingWriter(embeddingDAO.getDataSource());
-        System.out.println("Total Chuck Count: " +embeddingDAO.getTotalChunkCount());
+//        System.out.println("Total Chuck Count: " +embeddingDAO.getTotalChunkCount());
         List<Path> files = new ArrayList<>();
         try (Stream<Path> s = Files.walk(root)) {
             s.filter(Files::isRegularFile)
